178f0a046d630a8fdca97c671db7883de0b3b33c
benitairmadiani <benitairmadiani@users.noreply.github.com>
2020-12-17 12:33:55 +0700

    Update

--- atm_program.py
+++ atm_program.py
@@ -1,1 +1,38 @@
+import random, datetime
+from customer import Customer
+from atm_card import ATMCard
 
+customer1 = Customer(1234, 1234, 10000)
+
+while True:
+    id = int(input("Masukkan pin anda: "))
+    trial = 0
+
+    while (id != int(atm.checkPin()) and trial < 3):
+        id = int(input("Pin anda salah. Silahkan masukkan lagi: "))
+        trial +=1
+        if trial == 3:
+            print("Error. Silahkan ambil kartu dan coba lagi")
+
+    while True:
+        print("Selamat datang")
+        print("0: Cek saldo, 1: Debet, 2: Simpan, 3: Ganti pin, 4: Keluar")
+        selected_menu = int(input("Pilih menu di atas dalam (0-4): "))
+        
+        if selected_menu == 0:
+            customer.checkBalance()
+        
+        elif selected_menu == 1:
+            nominal = int(input("Masukkan nominal uang yang akan ditarik: "))
+            if nominal < custBalance:
+                customer.withdrawBalance()
+            else:
+                nominal = int(input("Saldo anda tidak mencukupi. Masukkan nominal uang yang akan ditarik: "))
+        
+        elif selected_menu == 2:
+            nominal = int(input("Masukkan nominal uang yang anda tambahkan: "))
+            customer.depositBalance()
+        
+        else:
+            menu = int(input("Pilih menu di atas dalam (0-4): "))
+     
--- customer.py
+++ customer.py
@@ -1,1 +1,17 @@
+from atm_card import ATMCard
 
+class Customer(ATMCard):
+    def __init__(self, id, custPin, custBalance):
+        super().__init(defaultPin, defaultBalance)
+        self.id = id
+    
+    def withdrawBalance(self, nominal):
+        self.nominal = nominal
+        return 'Saldo anda saat ini adalah Rp ' + str(self.custBalance - self.nominal)
+    
+    def depositBalance(self, nominal):
+        self.nominal = nominal
+        return 'Saldo anda saat ini adalah Rp ' + str(self.custBalance + self.nominal)
+    
+    def checkBalance(self):
+        return "Saldo anda saat ini adalah Rp" + str(self.custBalance)
